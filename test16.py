999945cbf738437fe3a226c4243e20ed4164f6f7
ECNUzhaoming <55483900+ECNUzhaoming@users.noreply.github.com>
2019-12-21 23:34:31 +0800

    Add files via upload

--- test16.py
+++ test16.py
@@ -1,98 +1,413 @@
 
 # coding: utf-8
 
-# In[2]:
+# In[3]:
 
-import re#处理字符串的模块，如查找特定字符，删除特定字符，字符串分割等
-import tkinter#Tkinter模块("Tk 接口")是Python的标准Tk GUI工具包的接口，位Python的内置模块，直接import tkinter即可使用。
-import tkinter.messagebox#调用tkinter模块中的messagebox函数，这个是消息框，对话框的关键，会弹出一个小框
- 
-#按钮操作，点击按钮后需要做的处理
-def buttonClik(btn):
-    content=contentVar.get()#获取文本框中的内容
-    #如果已有内容是以小数点开头的，在前面加0
-    if content.startswith('.'):
-        content='0'+content#字符串可以直接用+来增加字符
-    #根据不同的按钮作出不同的反应
-    if btn in '0123456789':
-        content+=btn#0-9中哪个键按下了，就在content字符串中增添
-    elif btn=='.':
-        #re.split，支持正则及多个字符切割
-        lastPart=re.split(r'\+|-|\*|/',content)[-1]#将content从+-*/这些字符的地方分割开来，[-1]表示获取最后一个字符
-        if '.'in lastPart:
-            tkinter.messagebox.showerror('错误','重复出现的小数点')#出现对话框，并提示信息
-            return
-        else:
-            content+=btn
-    elif btn=='C':
-        content=''#清除文本框
-    elif btn=='=':
-        try:
-            #对输入的表达式求值
-            content=str(eval(content))#调用函数eval，用字符串计算出结果
-        except:
-            tkinter.messagebox.showerror('错误','表达式有误')
-            return
-    elif btn in operators:
-        if content.endswith(operators):#如果content中最后出现的+-*/
-            tkinter.messagebox.showerror('错误','不允许存在连续运算符')
-            return
-        content+=btn
-    elif btn=='Sqrt':
-        n=content.split('.')#从.处分割存入n，n是一个列表
-        if all(map(lambda x:x.isdigit(),n)):#如果列表中所有的都是数字，就是为了检查表达式是不是正确的
-            content=eval(content)**0.5
-        else:
-            tkinter.messagebox.showerror('错误','表达式错误')
-            return
-    contentVar.set(content)#将结果显示到文本框中
- 
- 
-root=tkinter.Tk()#生成主窗口，用root表示，后面就在root操作
-#设置窗口大小和位置
-root.geometry('300x270+400+100')#指定主框体大小
-#不允许改变窗口大小
-root.resizable(False,False)#框体大小可调性，分别表示x,y方向的可变性；
-#设置窗口标题
-root.title('计算器')
- 
-#文本框和按钮都是tkinter中的组件
-#Entry        　　 文本框（单行）；
-#Button        　　按钮；
-#放置用来显示信息的文本框，设置为只读
-#tkinter.StringVar    能自动刷新的字符串变量，可用set和get方法进行传值和取值
-contentVar=tkinter.StringVar(root,'')
-contentEntry=tkinter.Entry(root,textvariable=contentVar)#括号里面，可见第一个都是root,即表示都是以root为主界面的，将文本框中的内容存在contentVar中
-contentEntry['state']='readonly'#文本框只能读，不能写
-contentEntry.place(x=10,y=10,width=280,height=20)#文本框在root主界面的xy坐标位置，以及文本框自生的宽和高
-#x:        　　　 组件左上角的x坐标；
-#y:        　　   组件右上角的y坐标；
-#放置清除按钮和等号按钮
-btnClear=tkinter.Button(root,text='C',bg = 'red',command=lambda:buttonClik('C'))#在root主界面中放置按钮，按钮上显示C，红色，点击按钮后进入buttonClik回调函数做进一步的处理，注意传入了参数‘C’，这样就能分清是哪个按钮按下了
-#下面的内容和上面的模式都是一样的
-btnClear.place(x=40,y=40,width=80,height=20)
-btnCompute=tkinter.Button(root,text='=',bg = 'yellow',command=lambda :buttonClik('='))
-btnCompute.place(x=170,y=40,width=80,height=20)
- 
-#放置10个数字、小数点和计算平方根的按钮
-digits=list('0123456789.')+['Sqrt']#序列list是Python中最基本的数据结构。序列中的每个元素都分配一个数字 - 它的位置，或索引，第一个索引是0，第二个索引是1，依此类推。
-index=0
-#用循环的方式将上面数字、小数点、平方根这12个按钮分成四行三列进行放置
-for row in range(4):
-    for col in range(3):
-        d=digits[index]#按索引从list中取值，和c语言中的数组类似
-        index+=1#索引号递增
-        btnDigit=tkinter.Button(root,text=d,command=lambda x=d:buttonClik(x))#和上面的是类似的
-        btnDigit.place(x=20+col*70,y=80+row*50,width=50,height=20)#很显然，每次放一个按钮的位置是不一样的，但是它们之间的关系时确定的
-#放置运算符按钮
-operators=('+','-','*','/','**','//')#Python的元组与列表类似，不同之处在于元组的元素不能修改。
-#元组使用小括号，列表使用方括号。
-#enumerate() 函数用于将一个可遍历的数据对象(如列表、元组或字符串)组合为一个索引序列，同时列出数据和数据下标，一般用在 for 循环当中。
-for index,operator in enumerate(operators):
-    btnOperator=tkinter.Button(root,text=operator,bg = 'orange',command=lambda x=operator:buttonClik(x))#创建的过程和上面类似
-    btnOperator.place(x=230,y=80+index*30,width=50,height=20)
- 
-root.mainloop()#进入消息循环（必需组件）
+from tkinter import *
+import random
+import time
+from tkMessageBox import *
+
+
+#俄罗斯方块界面的高度
+HEIGHT  = 20
+
+#俄罗斯方块界面的宽度
+WIDTH   = 10
+
+ACTIVE  = 1
+PASSIVE = 0
+TRUE    = 1
+FALSE   = 0
+
+style = [
+            [[(0,0),(0,1),(1,1),(2,1)],[(1,0),(1,1),(1,2),(0,2)],[(0,1),(1,1),(2,1),(2,2)],[(1,0),(2,0),(1,1),(1,2)]],#j
+            [[(1,0),(1,1),(1,2),(2,1)],[(1,0),(0,1),(1,1),(2,1)],[(1,0),(1,1),(1,2),(0,1)],[(0,1),(1,1),(2,1),(1,2)]],#T
+            [[(0,1),(1,1),(2,1),(2,0)],[(0,0),(1,0),(1,1),(1,2)],[(0,1),(1,1),(2,1),(0,2)],[(1,0),(1,1),(1,2),(2,2)]],#反L
+            [[(0,0),(0,1),(1,1),(1,2)],[(2,1),(1,1),(1,2),(0,2)],[(0,0),(0,1),(1,1),(1,2)],[(2,1),(1,1),(1,2),(0,2)]],#Z
+            [[(1,0),(1,1),(0,1),(0,2)],[(0,1),(1,1),(1,2),(2,2)],[(1,0),(1,1),(0,1),(0,2)],[(0,1),(1,1),(1,2),(2,2)]],#反Z
+            [[(0,0),(0,1),(1,1),(1,0)],[(0,0),(0,1),(1,1),(1,0)],[(0,0),(0,1),(1,1),(1,0)],[(0,0),(0,1),(1,1),(1,0)]],#田
+            [[(1,0),(1,1),(1,2),(1,3)],[(0,1),(1,1),(2,1),(3,1)],[(1,0),(1,1),(1,2),(1,3)],[(0,1),(1,1),(2,1),(3,1)]]#长条
+    ]
+
+root=Tk();
+root.title('俄罗斯方块')
+
+class App(Frame):
+    def __init__(self,master):
+        Frame.__init__(self)
+        master.bind('<Up>',self.Up)
+        master.bind('<Left>',self.Left)
+        master.bind('<Right>',self.Right)
+        master.bind('<Down>',self.Down)
+        
+        master.bind('<space>',self.Space)
+        master.bind('<Control-Shift-Key-F12>',self.Play)
+        master.bind('<Key-P>',self.Pause)
+        master.bind('<Key-S>',self.StartByS)
+        
+        # rgb颜色值
+        self.backg="#%02x%02x%02x" % (120,150,30)    #大背景
+        self.frontg="#%02x%02x%02x" % (40,120,150)    #下一个形状颜色
+        self.nextg="#%02x%02x%02x" % (150,100,100)    #小背景
+        self.flashg="#%02x%02x%02x" % (210,130,100)    #炸的颜色
+        
+        self.LineDisplay=Label(master,text='Lines: ',bg='black',fg='red')
+        self.Line=Label(master,text='0',bg='black',fg='red')
+        self.ScoreDisplay=Label(master,text='Score: ',bg='black',fg='red')
+        self.Score=Label(master,text='0',bg='black',fg='red')
+        self.SpendTimeDisplay=Label(master,text='Time: ',bg='black',fg='red')
+        self.SpendTime=Label(master,text='0.0',bg='black',fg='red')
+        
+        self.LineDisplay.grid(row=HEIGHT-2,column=WIDTH,columnspan=2)
+        self.Line.grid(row=HEIGHT-2,column=WIDTH+2,columnspan=3)
+        self.ScoreDisplay.grid(row=HEIGHT-1,column=WIDTH,columnspan=2)
+        self.Score.grid(row=HEIGHT-1,column=WIDTH+2,columnspan=3)
+        self.SpendTimeDisplay.grid(row=HEIGHT-4,column=WIDTH,columnspan=2)
+        self.SpendTime.grid(row=HEIGHT-4,column=WIDTH+2,columnspan=3)
+        
+        self.TotalTime=0.0
+        self.TotalLine=0
+        self.TotalScore=0
+        
+        #游戏结束
+        self.isgameover=FALSE
+        #暂停
+        self.isPause=FALSE
+        #开始
+        self.isStart=FALSE
+        self.NextList=[]        #整个小背景
+        self.NextRowList=[]     #一行小背景
+        
+        self.px=0
+        self.py=0       #记录方块参考点
+        
+        #渲染小背景
+        r=0;c=0
+        for k in range(4*4):
+            LN=Label(master,text='    ',bg=str(self.nextg),fg='white',relief=FLAT,bd=3)
+            LN.grid(row=r,column=WIDTH+c,sticky=N+E+S+W)
+            self.NextRowList.append(LN)
+            c=c+1
+            if c>=4:
+                r=r+1;c=0
+                self.NextList.append(self.NextRowList)
+                self.NextRowList=[]
+                
+        #渲染大背景
+        self.BlockList=[]
+        self.BlockRowList=[]
+        self.LabelList=[]
+        self.LabelRowList=[]
+        row=0;col=0
+        for i in range(HEIGHT*WIDTH):
+            L=Label(master,text='    ',bg=str(self.backg),fg='white',relief=FLAT,bd=4)
+            L.grid(row=row,column=col,sticky=N+E+S+W)
+            L.row=row;L.col=col;L.isactive=PASSIVE
+            self.BlockRowList.append(0);    #大背景每个格子初始化为0值
+            self.LabelRowList.append(L)
+            col=col+1
+            if col>=WIDTH:
+                row=row+1;col=0
+                self.BlockList.append(self.BlockRowList)
+                self.LabelList.append(self.LabelRowList)
+                self.BlockRowList=[]
+                self.LabelRowList=[]
+                
+        #file
+        fw=open('text.txt','a')
+        fw.close()
+        hasHead=FALSE
+        f=open('text.txt','r')
+        if f.read(5)=='score':
+            hasHead=TRUE
+        f.close()
+        self.file=open('text.txt','a')
+        if hasHead==FALSE:
+            self.file.write('score    line    time    scorePtime    linePtime    scorePline    date/n')
+            self.file.flush()
+            
+        self.time=1000
+        self.OnTimer()
+        
+    def __del__(self):
+        #self.file.close()
+        pass
+        
+    def Pause(self,event):
+        self.isPause=1-self.isPause
+        
+    def Up(self,event):
+        BL=self.BlockList   #格子的值
+        LL=self.LabelList   #格子Label
+        
+        Moveable=TRUE       #是否可旋转
+        
+        #代码编写开始
+        nowStyle = style[self.xnow][(self.ynow)]
+        newStyle = style[self.xnow][(self.ynow+1)%4]  #算出下一俄罗斯方块
+        self.ynow = (self.ynow+1)%4 #此行代码非常重要，否则响应UP时，只能变第一次
+        
+        print("nowStyle:"+str(nowStyle)+"=====>>newStyle:"+str(newStyle))
+        
+        #根据现有形状中每个label的坐标计算出旋转后目标坐标(x,y)
+        SourceList=[];DestList=[]
+        
+        for i in range(4):
+            SourceList.append([ nowStyle[i][0]+self.px, nowStyle[i][1]+self.py])
+            x = newStyle[i][0]+self.px
+            y = newStyle[i][1]+self.py
+            DestList.append([x, y])
+            
+            if x<0 or x>=HEIGHT or y<0 or y>=WIDTH : #or BL[x][y]==1 or LL[x][y].isactive==PASSIVE
+                Moveable=FALSE
+            
+        if Moveable==TRUE:
+            for i in range(len(SourceList)):
+                self.Empty(SourceList[i][0],SourceList[i][1])
+            for i in range(len(DestList)):
+                self.Fill(DestList[i][0],DestList[i][1])
+                
+    def Left(self,event):
+        BL=self.BlockList;LL=self.LabelList
+        Moveable=TRUE
+        for i in range(HEIGHT):
+            for j in range(WIDTH):
+                if LL[i][j].isactive==ACTIVE and j-1<0:Moveable=FALSE
+                if LL[i][j].isactive==ACTIVE and j-1>=0 and BL[i][j-1]==1 and LL[i][j-1].isactive==PASSIVE:Moveable=FALSE
+        if Moveable==TRUE:
+            self.py-=1
+            for i in range(HEIGHT):
+                for j in range(WIDTH):
+                    if j-1>=0 and LL[i][j].isactive==ACTIVE and BL[i][j-1]==0:
+                        self.Fill(i,j-1);self.Empty(i,j)
+                        
+    def Right(self,event):
+        BL=self.BlockList;LL=self.LabelList
+        Moveable=TRUE
+        for i in range(HEIGHT):
+            for j in range(WIDTH):
+                if LL[i][j].isactive==ACTIVE and j+1>=WIDTH:Moveable=FALSE
+                if LL[i][j].isactive==ACTIVE and j+1<WIDTH and BL[i][j+1]==1 and LL[i][j+1].isactive==PASSIVE:Moveable=FALSE
+        if Moveable==TRUE:
+            self.py+=1
+            for i in range(HEIGHT-1,-1,-1):
+                for j in range(WIDTH-1,-1,-1):
+                    if j+1<WIDTH and LL[i][j].isactive==ACTIVE and BL[i][j+1]==0:
+                        self.Fill(i,j+1);self.Empty(i,j)
+                        
+    def Down(self,event):
+        BL=self.BlockList;LL=self.LabelList
+        Moveable=TRUE
+        for i in range(HEIGHT):
+            for j in range(WIDTH):
+                if LL[i][j].isactive==ACTIVE and i+1>=HEIGHT:Moveable=FALSE
+                if LL[i][j].isactive==ACTIVE and i+1<HEIGHT and BL[i+1][j]==1 and LL[i+1][j].isactive==PASSIVE:Moveable=FALSE
+        if Moveable==TRUE and self.isStart :
+            self.px+=1
+            for i in range(HEIGHT-1,-1,-1):
+                for j in range(WIDTH-1,-1,-1):
+                    if i+1<HEIGHT and LL[i][j].isactive==ACTIVE and BL[i+1][j]==0:
+                        self.Fill(i+1,j);self.Empty(i,j);
+        if Moveable==FALSE:
+            for i in range(HEIGHT):
+                for j in range(WIDTH):
+                    LL[i][j].isactive=PASSIVE
+            self.JudgeLineFill()
+            self.Start()
+            if self.isgameover==TRUE:showinfo('T_T','The game is over!');self.Distroy();return FALSE
+            for i in range(4):
+                for j in range(4):
+                    self.NextEmpty(i,j)
+            self.Rnd()
+        return Moveable
+                        
+    def Space(self,event):
+        while 1:
+            if self.Down(0)==FALSE:break
+            
+    def OnTimer(self):
+        if self.isStart==TRUE and self.isPause==FALSE:
+            self.TotalTime = self.TotalTime + float(self.time)/1000
+            self.SpendTime.config(text=str(self.TotalTime))
+        
+        if self.isPause==FALSE:
+            self.Down(0)
+        if self.TotalScore>=1000:self.time=900
+        if self.TotalScore>=2000:self.time=750
+        if self.TotalScore>=3000:self.time=600
+        if self.TotalScore>=4000:self.time=400
+        self.after(self.time,self.OnTimer)      #随着分数增大，俄罗斯方块下降速度加快
+    
+    def JudgeLineFill(self):
+        BL=self.BlockList;LL=self.LabelList
+        count=0;LineList=[]
+        for i in range(WIDTH):LineList.append(1)
+        #display flash
+        for i in range(HEIGHT):
+            if BL[i]==LineList:
+                count=count+1
+                for k in range(WIDTH):
+                    LL[i][k].config(bg=str(self.flashg))
+                    LL[i][k].update()
+        if count!=0:self.after(100)
+        #delete block
+        for i in range(HEIGHT):
+            if BL[i]==LineList:
+                #count=count+1
+                for j in range(i,0,-1):
+                    for k in range(WIDTH):
+                        BL[j][k]=BL[j-1][k]
+                        LL[j][k]['relief']=LL[j-1][k].cget('relief')
+                        LL[j][k]['bg']=LL[j-1][k].cget('bg')
+                for l in range(WIDTH):
+                    BL[0][l]=0
+                    LL[0][l].config(relief=FLAT,bg=str(self.backg))
+        self.TotalLine=self.TotalLine+count
+        if count==1:self.TotalScore=self.TotalScore+1*WIDTH
+        if count==2:self.TotalScore=self.TotalScore+3*WIDTH
+        if count==3:self.TotalScore=self.TotalScore+6*WIDTH
+        if count==4:self.TotalScore=self.TotalScore+10*WIDTH
+        self.Line.config(text=str(self.TotalLine))
+        self.Score.config(text=str(self.TotalScore))
+        
+    def Fill(self,i,j):
+        if j<0:return
+        if self.BlockList[i][j]==1:self.isgameover=TRUE
+        self.BlockList[i][j]=1
+        self.LabelList[i][j].isactive=ACTIVE
+        self.LabelList[i][j].config(relief=RAISED,bg=str(self.frontg))
+        
+    def Empty(self,i,j):
+        self.BlockList[i][j]=0
+        self.LabelList[i][j].isactive=PASSIVE
+        self.LabelList[i][j].config(relief=FLAT,bg=str(self.backg))
+        
+    def Play(self,event):
+        showinfo('Made in China','^_^')
+        
+    def NextFill(self,i,j):
+        self.NextList[i][j].config(relief=RAISED,bg=str(self.frontg))
+        
+    def NextEmpty(self,i,j):
+        self.NextList[i][j].config(relief=FLAT,bg=str(self.nextg))
+        
+    def Distroy(self):
+        #save
+        if self.TotalScore!=0:
+            #cehkongfu
+            savestr='%-9u%-8u%-8.2f%-14.2f%-13.2f%-14.2f%s/n' % (
+                self.TotalScore,self.TotalLine,self.TotalTime
+               ,self.TotalScore/self.TotalTime
+               ,self.TotalLine/self.TotalTime
+               ,float(self.TotalScore)/self.TotalLine
+               ,time.strftime('%Y-%m-%d %H:%M:%S',time.localtime()))
+            self.file.seek(0,2)
+            self.file.write(savestr)
+            self.file.flush()
+        
+        for i in range(HEIGHT):
+            for j in range(WIDTH):
+                self.Empty(i,j)
+        self.TotalLine=0;self.TotalScore=0;self.TotalTime=0.0
+        self.Line.config(text=str(self.TotalLine))
+        self.Score.config(text=str(self.TotalScore))
+        self.SpendTime.config(text=str(self.TotalTime))
+        self.isgameover=FALSE
+        self.isStart=FALSE
+        self.time=1000
+        for i in range(4):
+            for j in range(4):
+                self.NextEmpty(i,j)
+                
+    #游戏开始方块
+    def Start(self):
+        nextStyle = style[self.x][self.y]   #下一形状
+        self.xnow = self.x
+        self.ynow = self.y          #记录大背景中的方块
+        self.py = random.randint(0,6)
+        print("给py赋任意值:"+str(self.py))
+        self.px = 0
+        for ii in range(4):
+            self.Fill(int(nextStyle[ii][0]),int(nextStyle[ii][1])+self.py)
+        self.isStart=TRUE   #游戏开始
+        
+    #预处理方块
+    def Rnd(self):
+        self.x=random.randint(0,6)
+        self.y=random.randint(0,3)
+        nextStyle = style[self.x][self.y]   #下一形状
+        for ii in range(4):
+            self.NextFill(int(nextStyle[ii][0]),int(nextStyle[ii][1]))
+        
+    #游戏开始给出一次任意形状的方块
+    def RndFirst(self):
+        self.x=random.randint(0,6)  #选择第一个方块style
+        self.y=random.randint(0,3)
+        
+    def Show(self):
+        self.file.seek(0)
+        strHeadLine=self.file.readline()
+        dictLine={}
+        strTotalLine=''
+        for OneLine in self.file.readlines():
+            temp=int(OneLine[:5])
+            dictLine[temp]=OneLine
+            
+        list=sorted(dictLine.items(),key=lambda d:d[0])
+        ii=0        
+        for onerecord in reversed(list):
+            ii=ii+1
+            if ii<11:
+                strTotalLine+=onerecord[1]
+        showinfo('Ranking', strHeadLine+strTotalLine)
+        
+    def StartByS(self,event):
+        self.RndFirst()
+        self.Start()
+        self.Rnd()
+        
+def Start():
+    app.RndFirst()
+    app.Start()
+    app.Rnd()
+     
+def End():
+    app.Distroy()
+    
+def Set():
+    print("设置功能待完善...")
+    
+def Show():
+    app.Show()
+
+#主菜单
+mainmenu=Menu(root)
+root['menu']=mainmenu
+
+#二级菜单：game
+gamemenu=Menu(mainmenu)
+mainmenu.add_cascade(label='游戏',menu=gamemenu)
+gamemenu.add_command(label='开始',command=Start)
+gamemenu.add_command(label='结束',command=End)
+gamemenu.add_separator()
+gamemenu.add_command(label='退出',command=root.quit)
+
+#二级菜单：set
+setmenu=Menu(mainmenu)
+mainmenu.add_cascade(label='设置',menu=setmenu)
+setmenu.add_command(label='设置',command=Set)
+
+#二级菜单：show
+showmenu=Menu(mainmenu)
+mainmenu.add_cascade(label='展示',menu=showmenu)
+showmenu.add_command(label='展示',command=Show)
+
+#绑定功能
+
+app=App(root)
+#程序入口
+root.mainloop()
 
 
 # In[ ]:
